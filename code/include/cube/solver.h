9e353a1a930295464513262a2c32e7c4cbe2a533
Gordon Guan <g2guan@edu.uwaterloo.ca>
2017-10-22 23:01:32 -0400

    solver: created rubik's cube definitions

---
+++ code/include/cube/solver.h
@@ -0,0 +1,8 @@
+#ifndef SOLVER_H
+#define SOLVER_H
+
+#include "cube/rubik.h"
+
+rubik_sequence_t rubik_solve(rubik_cube_t cube);
+
+#endif
--- code/src/main.c
+++ code/src/main.c
@@ -1,21 +1,7 @@
 #include <stdio.h>
 
-#include "ckociemba/search.h"
 
 int main(const int argc, const char *argv[]){
-    
-    // input parameters in ckociemba/facelet.h
-
-	char *facelets = "UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB";
-	char *sol = solution(
-		facelets,
-		24,
-		1000,
-		0,
-		"cache"
-	);
-	
-	printf("%s\n", sol);
 	
 	return 0;
 
