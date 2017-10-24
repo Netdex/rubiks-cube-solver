53b534a63f2db357db8c562899a5345fb575f7e9
Callum Moseley <callum.moseley1@gmail.com>
2017-10-23 22:23:35 -0400

    Added live updating loop showing the current cube face that can be seen

--- code/src/scanner_test.c
+++ code/src/scanner_test.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "cube_scanner.h"
 
 int main(void) {
     char colours[6] = "WYGBRO";
     int face[3][3] = {0};
-    scan_face("../rubik_face.jpg", face);
-    printf("%c %c %c\n", colours[face[0][0]], colours[face[0][1]], colours[face[0][2]]);
-    printf("%c %c %c\n", colours[face[1][0]], colours[face[1][1]], colours[face[1][2]]);
-    printf("%c %c %c\n", colours[face[2][0]], colours[face[2][1]], colours[face[2][2]]);
+    for (int i = 0; i < 20; i++) {
+        system("fswebcam --device /dev/video1 -q -r 640x360 --no-banner -S 30 temp.jpg");
+        scan_face("temp.jpg", face);
+        printf("\033[F\033[J\033[F\033[J\033[F\033[J");
+        printf("%c %c %c\n", colours[face[0][0]], colours[face[0][1]], colours[face[0][2]]);
+        printf("%c %c %c\n", colours[face[1][0]], colours[face[1][1]], colours[face[1][2]]);
+        printf("%c %c %c\n", colours[face[2][0]], colours[face[2][1]], colours[face[2][2]]);
+    }
     return 0;
 }
