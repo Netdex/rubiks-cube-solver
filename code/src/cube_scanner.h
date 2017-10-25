39b4169b6b9699a8f50870fdaa0062966578fa87
Callum Moseley <callum.moseley1@gmail.com>
2017-10-25 14:32:21 -0400

    Started converting colour recognition code to use HSV rather than RGB

--- code/src/cube_scanner.h
+++ code/src/cube_scanner.h
@@ -20,16 +20,47 @@
 #define HEIGHT 360
 
 // Colour definitions
-#define WHITE 0xffffff  // 0
-#define YELLOW 0xa8a061 // 1
-#define GREEN 0x006e39  // 2
-#define BLUE 0x00248D   // 3
-#define RED 0x921618    // 4
-#define ORANGE 0xB94D27 // 5
+//#define WHITE 0xffffff  // 0
+//#define YELLOW 0xa8a061 // 1
+//#define GREEN 0x006e39  // 2
+//#define BLUE 0x00248D   // 3
+//#define RED 0x921618    // 4
+//#define ORANGE 0xB94D27 // 5
+
+typedef struct {
+    double hue;
+    double sat;
+    double val;
+} hsv_colour;
+
+//const hsv_colour WHITE_MIN = {0.0, 0.0, 0.85};
+//const hsv_colour WHITE_MAX = {360.0, 0.15, 1.0};
+//
+//const hsv_colour YELLOW_MIN = {53.0, 0.5, 0.75};
+//const hsv_colour YELLOW_MAX = {75.0, 1.0, 1.0};
+//
+//const hsv_colour GREEN_MIN = {90.0, 0.5, 0.4};
+//const hsv_colour GREEN_MAX = {170.0, 1.0, 1.0};
+//
+//const hsv_colour BLUE_MIN = {200.0, 0.5, 0.4};
+//const hsv_colour BLUE_MAX = {270.0, 1.0, 1.0};
+//
+//const hsv_colour RED_MIN = {340.0, 0.5, 0.4};
+//const hsv_colour RED_MAX = {370.0, 1.0, 1.0};
+//
+//const hsv_colour ORANGE_MIN = {18.0, 0.5, 0.4};
+//const hsv_colour ORANGE_MAX = {45.0, 1.0, 1.0};
+
+const hsv_colour WHITE = {0.0, 0.0, 1.0};
+const hsv_colour YELLOW = {60.0, 1.0, 1.0};
+const hsv_colour GREEN = {120.0, 1.0, 1.0};
+const hsv_colour BLUE = {240.0, 1.0, 1.0};
+const hsv_colour RED = {0.0, 1.0, 1.0};
+const hsv_colour ORANGE = {32.0, 1.0, 1.0};
 
 void scan_face(char* imageLoc, int output[3][3]);
 int average_region(uint8_t* image, int image_w, int image_h, int image_bpp, int centre_x, int centre_y, int width, int height);
-int compare_colour(int colour1, int colour2);
-int determine_colour(int colour);
+int compare_colour(hsv_colour colour1, hsv_colour colour2);
+int determine_colour(hsv_colour colour);
 
 #endif
