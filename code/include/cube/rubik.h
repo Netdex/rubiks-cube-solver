4d79b3e00fbc787da9bbbe14a15544b9fa4f9f0f
RobbieZhuang <robbie.zhuang@gmail.com>
2017-10-26 01:31:58 -0400

    rubiks communication: added in first stage algorithm to remove instructions for top and bottom faces, added new enum for rotating the cube, added no change enum for side and direction, modified rubik_sequence_to_string to print updated sequence

--- code/include/cube/rubik.h
+++ code/include/cube/rubik.h
@@ -1,8 +1,6 @@
 #ifndef RUBIK_H
 #define RUBIK_H
 
-#include <stdbool.h>
-
 typedef enum {
    R_W = 0,
    R_G = 1,
@@ -26,26 +24,38 @@ typedef struct {
 } rubik_cube_t;
 
 typedef enum {
-    R_UP    = 0, 
+    R_UP    = 0,
     R_RIGHT = 1,
     R_FRONT = 2,
-    R_DOWN  = 3, 
-    R_LEFT  = 4, 
-    R_BACK  = 5
+    R_DOWN  = 3,
+    R_LEFT  = 4,
+    R_BACK  = 5,
+    R_NOSIDE = 6
 } rubik_side_t;
 
 typedef enum {
-    R_CW = 1, 
-    R_CCW = -1, 
-    R_DOUBLE_CW = 2
+    R_CW = 1,
+    R_CCW = -1,
+    R_DOUBLE_CW = 2,
+    R_NODIR = 3
 } rubik_dir_t;
+
+// Rotate
+typedef enum {
+    R_FONB = 0,
+    R_BONB = 1,
+    R_RONB = 2,
+    R_LONB = 3,
+    R_NOROT = 4
+} rubik_rt_t;
 
 /* elementary rubik's cube operation */
 typedef struct {
     rubik_side_t side;
     /* M': -1, M: 1, M2: 2 */
     rubik_dir_t direction;
-    bool is_rotation;
+
+    rubik_rt_t rotation;
 } rubik_op_t;
 
 /* sequence of rubik's cube operations */
@@ -73,10 +83,4 @@ char* rubik_sequence_to_string(rubik_sequence_t *t);
  * Convert cube into format compatible with solver API
  */
 char* rubik_convert_facelet(rubik_cube_t c);
-
-/*
- * Rotate face according to operation
- */
-rubik_face_t rubik_face_rotate(rubik_face_t face, rubik_dir_t dir);
-
 #endif
