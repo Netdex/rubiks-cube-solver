e4b178dec71819fc463ba44997fced50917bfbea
Gordon Guan <g2guan@edu.uwaterloo.ca>
2017-10-26 09:29:08 -0400

    cv: fix broken kmeans

--- code/include/cv/graphics.h
+++ code/include/cv/graphics.h
@@ -13,6 +13,7 @@ typedef struct {
 image_t gfx_create_image(int width, int height);
 void gfx_rect(image_t img, int x, int y, int w, int h, rgb_t c);
 void gfx_circle(image_t img, int x, int y, int r, rgb_t c);
+void gfx_line(image_t img, int x1, int y1, int x2, int y2, rgb_t c);
 
 void gfx_convert_buf(image_t img, uint8_t *out);
 
--- code/src/test/test_kmeans.c
+++ code/src/test/test_kmeans.c
@@ -1,34 +1,67 @@
 #include "stdafx.h"
 #include "cv/TinyPngOut.h"
+#include "cv/graphics.h"
+#include "cv/cluster.h"
 
 void test_kmeans(){
-    // Sample image data:
-	//   [red    , green , blue]
-	//   [magenta, yellow, cyan]
-	const int width = 3;
-	const int height = 2;
-	uint8_t pixels[] = {
-		0xFF, 0x00, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00, 0xFF,
-		0xFF, 0x00, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF,
-	};
-	
-	// Initialize file and Tiny Png Output
-	FILE *fout = fopen("demo-rgb.png", "wb");
+    rgb_t dataset[512];
+    for(int i = 0; i < 512; i++){
+        dataset[i].r = randf();
+        dataset[i].g = randf();
+        dataset[i].b = randf();
+    }
+
+    rgb_t class_col[] = {
+        {1.f, 0.f, 0.f},
+        {0.f, 1.f, 0.f},
+        {0.f, 0.f, 1.f},
+        {1.f, 1.f, 1.f},
+        {1.f, .5f, 0.f},
+        {1.f, 1.f, 0.f}
+    };
+
+    rgb_t centroids[6];
+    cluster_rgb_kmeans(6, 0.0001, 1000, dataset, 512, centroids);
+    int labels[512];
+    cluster_rgb_label(6, dataset, 512, centroids, labels);
+
+    const int width = 512, height = 512;
+    LOG("create image context");
+    image_t image = gfx_create_image(width, height);
+    
+    for(int i = 0; i < 512; i++){
+        gfx_rect(image, 
+            (int)(dataset[i].r * 512) - 2, 
+            (int)(dataset[i].g * 512) - 2, 
+            5,5, 
+            (rgb_t){
+                dataset[i].b,
+                dataset[i].b,
+                dataset[i].b
+        });
+        gfx_rect(image, 
+            (int)(dataset[i].r * 512)-1, 
+            (int)(dataset[i].g * 512)-1, 
+            3,3, 
+            class_col[labels[i]]
+        );
+    }
+
+    uint8_t *pixels = malloc(3 * width * height);
+    gfx_convert_buf(image, pixels);
+    
+	FILE *fout = fopen("output.png", "wb");
 	struct TinyPngOut pngout;
 	if (fout == NULL || TinyPngOut_init(&pngout, fout, width, height) != TINYPNGOUT_OK)
 		goto error;
-	
-	// Write image data
 	if (TinyPngOut_write(&pngout, pixels, width * height) != TINYPNGOUT_OK)
 		goto error;
-	
-	// Check for proper completion
 	if (TinyPngOut_write(&pngout, NULL, 0) != TINYPNGOUT_DONE)
 		goto error;
-	fclose(fout);
+    fclose(fout);
+    free(pixels);
 	return;
 error:
-	fprintf(stderr, "Error\n");
 	if (fout != NULL)
 		fclose(fout);
 }
