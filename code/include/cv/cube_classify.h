d4485e824a726f9f4a5ee4e5c67a46e0fd79d482
Gordon Guan <g2guan@edu.uwaterloo.ca>
2017-10-26 09:40:58 -0400

    cv: implemented clustering color classifier

---
+++ code/include/cv/cube_classify.h
@@ -0,0 +1,15 @@
+#ifndef CUBE_CLASSIFY_H
+#define CUBE_CLASSIFY_H
+
+#include "cube/rubik.h"
+#include "cv/color.h"
+
+/*
+ * Builds a cube from average color values of cube,
+ * clustering with k-memes
+ * 
+ * Face order is U, R, F, D, L, B
+ */
+rubik_cube_t cube_classify_from_colors(rgb_t colors[6][3][3]);
+
+#endif
