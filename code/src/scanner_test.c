ef9b15a658de63bc53d90362836dc2f51f15a6bd
Gordon Guan <g2guan@edu.uwaterloo.ca>
2017-11-15 13:02:32 -0500

    did not merge properly

--- code/src/scanner_test.c
+++ code/src/scanner_test.c
@@ -4,27 +4,15 @@
 #include <stdlib.h>
 #include <BBBiolib.h>
 #include "cube_scanner.h"
-<<<<<<< HEAD
-#include "bbb_gpio.h"
-#include "cube/rubik.h"
-#include "cube/solver.h"
-=======
 #include "lcd.h"
 #include "motor.h"
->>>>>>> 9e10252d0dd8aebbadfbbed4fe18c5842e3d6da2
 
 void test_kmeans();
 
 int main(void) {
-<<<<<<< HEAD
-    // srand((unsigned int)time(NULL));
-
-    // test_kmeans();
-=======
     //srand((unsigned int)time(NULL));
 
     //test_kmeans();
->>>>>>> 9e10252d0dd8aebbadfbbed4fe18c5842e3d6da2
     // char colours[6] = "WYGBRO";
     // int face[3][3] = {0};
 
@@ -36,19 +24,6 @@ int main(void) {
     //     printf("%c %c %c\n", colours[face[1][0]], colours[face[1][1]], colours[face[1][2]]);
     //     printf("%c %c %c\n", colours[face[2][0]], colours[face[2][1]], colours[face[2][2]]);
     // }
-<<<<<<< HEAD
-    char cube_mat[6][3][3];
-    for(int f = 0; f < 6; f++){
-        for(int y = 0; y < 3; y++){
-            for(int x = 0; x < 3; x++){
-                scanf("%c", &cube_mat[f][y][x]);
-            }
-        }
-    }
-    rubik_cube_t cube = rubik_make_cube_from_char(cube_mat);
-    rubik_solution_t solution = rubik_solve(cube);
-    printf("%d | %s\n", solution.error_code, solution.str);
-=======
 
     /*lcd l = {66, 67, 69, 68, 45, 44};
     lcd_init(l);
@@ -77,6 +52,5 @@ int main(void) {
 
     iolib_free();
     
->>>>>>> 9e10252d0dd8aebbadfbbed4fe18c5842e3d6da2
     return 0;
 }
