941ba53f1c8c9109d897992b1da788f08f788a04
Gordon Guan <g2guan@edu.uwaterloo.ca>
2017-11-17 16:47:31 -0500

    documentation

--- code/src/motor_op.c
+++ code/src/motor_op.c
@@ -2,29 +2,23 @@
 #include "motor_op.h"
 #include "motor.h"
 
+/* DOCUMENTATION IS IN motor_op.h */
+
+/* GPIO pinout for motors */
 motor motors[] = {
     {0,0,0,0},     // F
     {0,0,0,0},     // B
     {0,0,0,0},     // L
     {0,0,0,0},     // R
-    {0,0,0,0},     // L/R release
-    {0,0,0,0},     // F/B release
+    {0,0,0,0},     // L/R retract/extend
+    {0,0,0,0},     // F/B retract/extend
 };
 
 void motor_op_rotate_face(rubik_side_t face, rubik_dir_t dir){
-
-    switch(face){
-        case R_FRONT:
-        break;
-        case R_BACK:
-        break;
-        case R_LEFT:
-        break;
-        case R_RIGHT:
-        break;
-    }
+    assert(face != R_UP && face != R_DOWN);
+    
 }
 
 void motor_op_rotate_cube(rubik_side_t bottom){
-    assert(bottom != R_UP && bottom != R_DOWN);
+    
 }
